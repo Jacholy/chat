8486c36b1e9a0201f2de0e62810bec9e6d6c415c
Jacholy <Jacholy@163.com>
2023-04-13 20:08:50 +0800

    update key

--- chat.py
+++ chat.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
